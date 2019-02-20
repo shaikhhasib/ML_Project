b403faccd26b9dd7d955a3718cd4b1773140cab5
Muhammad Hasib <hasibdelhi@gmail.com>
2019-02-20 12:16:24 +0100

    Setup is Done

---
+++ Data-Analysis/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='Data_Analysis',
+	version='0.0.1',
+	url='www.github.com/MuhammadHasib/ML_Project',
+	license='BSD',
+	author='Muhammad Hasib',
+	packages=find_packages(),
+	install_requires=['PyQt5','pandas','sqlalchemy','nltk','numpy','jupyter','python-twitter'],
+	entry_points={},
+	extras_require={'dev': ['flake8',]},
+	)
