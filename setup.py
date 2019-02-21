8b74275546c260beac2c561528f52bdb0a592a0e
Muhammad Hasib <hasibdelhi@gmail.com>
2019-02-21 11:37:02 +0100

    Bug Fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 	license='BSD',
 	author='Muhammad Hasib',
 	packages=find_packages(),
-	install_requires=['PyQt5','pandas','sqlalchemy','nltk','numpy','jupyter','python-twitter'],
+	install_requires=['PyQt5','pandas','sqlalchemy','nltk','numpy','jupyter','python-twitter', 'tweepy'],
 	entry_points={},
 	extras_require={'dev': ['flake8',]},
 	)
